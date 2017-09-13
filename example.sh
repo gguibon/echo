7f851afc3ea86c6b714e35f1d12ec556fac38e01
gguibon <gael.guibon@gmail.com>
2017-09-13 18:58:12 +0200

    adding a shell script for example usages

---
+++ example.sh
@@ -0,0 +1,8 @@
+## example running it programmatically
+## need python 2
+## see requirements.txt for all requirements... Obviously
+python example.py
+
+
+## an example of training command (work in progress)
+# python sent_analysis.py -train corpus/twitter-train-cleansed-B.txt -c tw -f pol -t -o youpi
